f8669f20084fff43f8f824fe3c534fa11de9a1fb
Alperen Yildirim <alperen.yildirim@basistechnologies.com>
2026-04-21 09:12:18 +0000

    Second

--- srv/srv/srv/cat-service.js
+++ srv/srv/srv/cat-service.js
@@ -1,3 +1,4 @@
 module.exports = (srv) => {
-    srv.on('hello', () => "Hello from BTP Transported App!");
+    srv.on('hello', () => "Hello from BTP Transported App!")
+    srv.on('hello', () => "Hello again for the second commit");
 };
